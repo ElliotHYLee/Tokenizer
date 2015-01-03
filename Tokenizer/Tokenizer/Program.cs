2e2b8b3a55fcc3e893c5d0b3e1631c239dde6f7c
ElliotHYLee <hylee101001@gmail.com>
2015-01-03 00:54:46 -0500

    dd

--- Tokenizer/Tokenizer/Program.cs
+++ Tokenizer/Tokenizer/Program.cs
@@ -8,8 +8,20 @@ namespace Tokenizer
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            string str = "az8158][gx-5][gy-5][gz-5][ax317][ay6][az8177][gx-5][gy-5][gz-5][ax311][ay9][az8190][gx-5][gy-5][gz-4][ax341][ay6][az8164][gx-4][gy-4][gz-5][ax342][ay4][az8170][gx-4][gy-5][gz-5][ax328][ay1][az8171][gx-4][gy-4][gz-4][ax327][ay1][az8207][gx-4][gy-4][gz-4][ax335][ay-9][az8195][gx-5][gy-4][gz-4][ax328][ay-9][az8165][gx-5][gy-4][gz-4][ax324][ay-20][az8167][gx-5][gy-5][gz-4][ax328][ay-12][az8171][gx-5][gy-5][gz-4][ax329][ay4][az8184][gx-5][gy-5][gz-4][ax339][ay11][az8179][gx-4][gy-5][gz-5][ax338][ay-13][az8";
+            str = Tokenizer.trim(str);
+            Console.WriteLine(str);
+            List<string> test = Tokenizer.getTokens(str);
+            for (int i = 0; i < test.Count; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+
+            test.Clear();
         }
     }
 }
---
+++ Tokenizer/Tokenizer/Tokenizer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokenizer
+{
+    static class Tokenizer
+    {
+        public static string trim(string x)
+        {
+            int beginPos = findFirstChar(x, '[', true);
+            int endPos = findFirstChar(x, ']', false);
+            string result = x.Substring(beginPos, endPos - beginPos +1);
+            return result;
+        }
+
+        private static int findFirstChar(string str, char x, bool fromFront)
+        {
+            int result = 0;
+            if (fromFront)
+            {
+                result = str.IndexOf(x.ToString());
+            }
+            else
+            {
+                string revStr = reverse(str);
+                int position = revStr.IndexOf(x.ToString()) + 1;
+                result = str.Length - position;
+            }
+            return result;
+        }
+
+        private static string reverse(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        public static List<string> getTokens(string str)
+        {
+            List<string> result = new List<string>();
+            int beginPos = findFirstChar(str, '[', true);
+            int endPos = findFirstChar(str, ']', true);
+            string temp = null;
+            while (str.Length > 0)
+            {
+                // get first token of the string
+                temp = str.Substring(beginPos + 1, endPos - beginPos-1);
+                result.Add(temp);
+                // prepare the next string
+                str = str.Substring(endPos + 1, str.Length - endPos - 1);
+                beginPos = findFirstChar(str, '[', true);
+                endPos = findFirstChar(str, ']', true);
+            }
+            return result;
+        }
+
+    }
+}
